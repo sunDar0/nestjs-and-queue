edff5b1aab9c861ac7f0e91809b4f3218d3d3116
darr <36463655+sunDar0@users.noreply.github.com>
2021-11-16 08:54:15 +0900

    환경변수화
    환경변수화

--- main/src/listener.ts
+++ main/src/listener.ts
@@ -6,7 +6,7 @@ async function bootstrap(){
     const app = await NestFactory.createMicroservice(AppModule,{
         transport: Transport.RMQ,
         options: {
-          urls: ['amqps://kggdvjio:jSb0_R-SpaVTv-gE3XjpHWIIdHi0hXQH@dingo.rmq.cloudamqp.com/kggdvjio'],
+          urls: [process.env.RMQ_URL],
           queue: 'main_queue',
           queueOptions: {
             durable: false
